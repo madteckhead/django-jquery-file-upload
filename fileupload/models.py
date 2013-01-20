fb014751bc969f649ed15b95187780ed1bf1fc2c
Nathan Keilar <nathank@huntedhive.com>
2013-01-19 21:28:20 -0800

    associate user with Picture

--- fileupload/models.py
+++ fileupload/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Picture(models.Model):
     # This is a small demo using FileField instead of ImageField, not
     # depending on PIL. You will probably want ImageField in your app.
     file = models.ImageField(upload_to="pictures")
     slug = models.SlugField(max_length=50, blank=True)
+    creator = models.ForeignKey(User)
 
     def __unicode__(self):
         return self.file
