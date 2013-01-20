89befabb8417e7be32eefb0a1ade7185585ae051
Nathan Keilar <nathank@huntedhive.com>
2013-01-19 21:38:53 -0800

    white space updates

--- fileupload/urls.py
+++ fileupload/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls.defaults import *
-from fileupload.views import PictureCreateView, PictureDeleteView
+from fileupload.views import PictureCreateView, PictureDeleteView, MultiUploderView
+
 
 urlpatterns = patterns('',
     (r'^new/$', PictureCreateView.as_view(), {}, 'upload-new'),
+    (r'^new/add/$', MultiUploderView.as_view(), {}, 'upload-add'),
     (r'^delete/(?P<pk>\d+)$', PictureDeleteView.as_view(), {}, 'upload-delete'),
 )
 
--- fileupload/views.py
+++ fileupload/views.py
@@ -1,11 +1,12 @@
-from fileupload.models import Picture
 from django.views.generic import CreateView, DeleteView
+from django.views.generic.base import View
 
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, Http404
 from django.utils import simplejson
 from django.core.urlresolvers import reverse
-
 from django.conf import settings
+
+from fileupload.models import Picture
 
 def response_mimetype(request):
     if "application/json" in request.META['HTTP_ACCEPT']:
@@ -19,10 +20,80 @@ class PictureCreateView(CreateView):
     def form_valid(self, form):
         self.object = form.save()
         f = self.request.FILES.get('file')
-        data = [{'name': f.name, 'url': settings.MEDIA_URL + "pictures/" + f.name.replace(" ", "_"), 'thumbnail_url': settings.MEDIA_URL + "pictures/" + f.name.replace(" ", "_"), 'delete_url': reverse('upload-delete', args=[self.object.id]), 'delete_type': "DELETE"}]
+        data = [{'name': f.name, 'url': settings.MEDIA_URL + "pictures/" + f.name.replace(" ", "_"),
+                 'thumbnail_url': settings.MEDIA_URL + "pictures/" + f.name.replace(" ", "_"),
+                 'delete_url': reverse('upload-delete', args=[self.object.id]), 'delete_type': "DELETE"}]
         response = JSONResponse(data, {}, response_mimetype(self.request))
         response['Content-Disposition'] = 'inline; filename=files.json'
         return response
+
+
+class MultiUploderView(View):
+    model = Picture
+
+    def post(self, request, *args, **kwargs):
+        if request.FILES == None:
+            raise Http404("No objects uploaded")
+        file = request.FILES['file']
+
+        url = self.save_form(request, file, *args, **kwargs)
+
+        result = [{'name': file.name,
+                   'size': file.size,
+                   'url': url,
+                   'thumbnail_url': url,
+                   }, ]
+
+        response_data = simplejson.dumps(result)
+        if "application/json" in request.META['HTTP_ACCEPT_ENCODING']:
+            mimetype = 'application/json'
+        else:
+            mimetype = 'text/plain'
+        return HttpResponse(response_data, mimetype=mimetype)
+
+    def save_form(self, request, file, *args, **kwargs):
+        a = self.model()
+        a.creator = request.user
+        a.file.save(file.name, file)
+        a.save()
+        return a.file.url
+
+    def get(self, request, *args, **kwargs):
+        """
+        required to satisfy jquery upload get request
+        """
+        return HttpResponse('Only POST accepted')
+
+#    def get(self, request, *args, **kwargs):
+#        """
+#        required to satisfy jquery upload get request for existing images
+#        """
+#        try:
+#            recipe = Recipe.objects.get(id=self.kwargs['recipe_id'])
+#        except Recipe.DoesNotExist:
+#            raise Http404("Recipe does not exist, thus can't get images")
+#
+#        #        import ipdb; ipdb.set_trace()
+#        images = recipe.images.all()
+#
+#        #generating json response array
+#        result = []
+#
+#        for image in images:
+#            result.append({"name":image.description,
+#                           "size":image.file.size,
+#                           "url":image.file.url,
+#                           "thumbnail_url":image.file.url,})
+#
+#        response_data = simplejson.dumps(result)
+#
+#        #checking for json data type
+#        #big thanks to Guy Shapiro
+#        if "application/json" in request.META['HTTP_ACCEPT_ENCODING']:
+#            mimetype = 'application/json'
+#        else:
+#            mimetype = 'text/plain'
+#        return HttpResponse(response_data, mimetype=mimetype)
 
 
 class PictureDeleteView(DeleteView):
@@ -44,6 +115,7 @@ class PictureDeleteView(DeleteView):
 
 class JSONResponse(HttpResponse):
     """JSON response class."""
-    def __init__(self,obj='',json_opts={},mimetype="application/json",*args,**kwargs):
-        content = simplejson.dumps(obj,**json_opts)
-        super(JSONResponse,self).__init__(content,mimetype,*args,**kwargs)
+
+    def __init__(self, obj='', json_opts={}, mimetype="application/json", *args, **kwargs):
+        content = simplejson.dumps(obj, **json_opts)
+        super(JSONResponse, self).__init__(content, mimetype, *args, **kwargs)
