fde62923e1c3a598356ae6ab4057a70bbd813747
Nathan Keilar <nathank@huntedhive.com>
2013-01-19 21:26:59 -0800

    change to ImageField, not FileField

--- fileupload/models.py
+++ fileupload/models.py
@@ -1,13 +1,8 @@
 from django.db import models
 
 class Picture(models.Model):
-
-    # This is a small demo using just two fields. The slug field is really not
-    # necessary, but makes the code simpler. ImageField depends on PIL or
-    # pillow (where Pillow is easily installable in a virtualenv. If you have
-    # problems installing pillow, use a more generic FileField instead.
-
-    #file = models.FileField(upload_to="pictures")
+    # This is a small demo using FileField instead of ImageField, not
+    # depending on PIL. You will probably want ImageField in your app.
     file = models.ImageField(upload_to="pictures")
     slug = models.SlugField(max_length=50, blank=True)
 
